421d43da17c7aad34ad0981a820107571c28ddd4
Eibriel <eibriel@yahoo.com.ar>
2015-03-11 22:00:20 +0100

    Updated Version

--- addons/blender/eibriel_monitor/__init__.py
+++ addons/blender/eibriel_monitor/__init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "eMonitor",
     "author": "Eibriel",
-    "version": (0, 2, 1),
+    "version": (0, 2, 2),
     "blender": (2, 73, 0),
     "location": "Image > Send to eMonitor",
     "description": "Monitor your render on the Web",
